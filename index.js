3d6efbcd8fa7d4ba7d94ec01c89d4a8ba0679108
garrepi <jacgarrett18@gmail.com>
2022-10-05 15:49:49 -0400

    basic name storage and retrevial

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs').promises;
 const qs = require('node:querystring');
+const { MongoClient } = require('mongodb');
+const mongoUrl = "mongodb://localhost:27017/bstb";
+var db;
 
 const host = 'localhost';
 const port = 8000;
+
+MongoClient.connect(mongoUrl, function (err, client) {
+    if (err) {
+        throw err;
+    } else {
+        db = client.db('bstb');
+        console.log('Connected to MongoDB');
+        startServer();
+    }
+});
 
 const requestListener = (req, res) => {
     const reqUrl = url.parse(req.url);
@@ -14,6 +27,9 @@ const requestListener = (req, res) => {
             break
         case "/register":
             register(req, res);
+            break
+        case "/guests":
+            guests(req, res);
             break
         default:
             res.writeHead(404);
@@ -36,20 +52,52 @@ const index = (req, res) => {
     });
 }
 
+
 const register = (req, res) => {
     const reqUrl = url.parse(req.url);
     //res.setHeader("Content-Type", "application/json");
     res.writeHead(200);
     const queries = qs.parse(reqUrl.query);
-    const fname = queries.fname;
-    const lname = queries.lname;
+    const guest = {
+        ...queries,
+        date: Date.now()
+    }
 
-    console.log(fname, lname);
+    db.collection('guests').insertOne(guest, function (err, res) {
+        if (err) {
+            console.error(err)
+        } else {
+            console.log(res)
+        }
+    })
+
     res.end(JSON.stringify(queries));
 }
 
-const server = http.createServer(requestListener);
+// return json of all guests on GET
+const guests = (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    const options = {
+        // TODO: date
+   //   sort: { date: 1 },
+      projection: { _id: 0, fname: 1, lname: 1 },
+    };
 
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+    const entries = db.collection("guests").find({}, options);
+    entries.toArray().then(array => {
+        res.writeHead(200);
+        res.end(JSON.stringify(array))
+    }).catch(err => {
+        console.error(err);
+        res.writeHead(500);
+        res.end("{}")
+    })
+}
+
+const startServer = () => {
+    const server = http.createServer(requestListener);
+
+    server.listen(port, host, () => {
+        console.log(`Server is running on http://${host}:${port}`);
+    });
+};
